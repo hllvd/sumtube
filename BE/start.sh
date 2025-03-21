e480b9d47c30db0c42a0feb19fee29e761eba486
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2025-03-21 17:05:18 -0300

    building structure and packing all into a docker-compose

--- BE/start.sh
+++ BE/start.sh
@@ -8,6 +8,7 @@ fi
 # Kill any process using port 8080
 echo "Killing processes using port 8080..."
 lsof -ti :8080 | xargs -r kill -9
+lsof -ti :8081 | xargs -r kill -9
 
 echo "Running docker-compose up -d"
 # Start Docker Compose
