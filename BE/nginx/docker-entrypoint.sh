59a939018888183945bdfca9aab215fac76afb29
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2025-08-24 17:04:18 -0300

    remove import file

--- BE/nginx/docker-entrypoint.sh
+++ BE/nginx/docker-entrypoint.sh
@@ -54,9 +54,6 @@ server {
     listen 443 ssl;
     server_name $DOMAIN $API_SUBDOMAIN;
 
-    #ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
-    #ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
-
     location / {
         proxy_pass http://${GO_SERVER_HOST}:${GO_SERVER_PORT};
         proxy_set_header Host \$host;
