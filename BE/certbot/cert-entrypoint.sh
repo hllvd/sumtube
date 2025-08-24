d52451987db15a1709bba6b2e507903eda628f85
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2025-08-24 16:55:34 -0300

    modify cert container

---
+++ BE/certbot/cert-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Processing Certbot..."
+if [ \"$ENABLE_HTTPS\" = 'true' ]; then
+    certbot certonly --webroot -w /app/static -d sumtube.io -d api.sumtube.io --email vandal.hudson@gmail.com --agree-tos --non-interactive;
+else
+    echo "Skipping certbot (dev mode)";
+fi
