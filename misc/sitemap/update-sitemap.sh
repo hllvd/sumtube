e3bc61b94eb56cf07a3447b5b16f56355ba123b6
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2025-10-03 12:15:28 -0300

    adjust update-sitemap

--- misc/sitemap/update-sitemap.sh
+++ misc/sitemap/update-sitemap.sh
@@ -8,9 +8,9 @@ go run main.go copy-es && \
 go run main.go copy-fr && \
 go run main.go copy-it
 
-# 2. Add ONLY sitemap files (adjust path if needed)
+# 2. Add ONLY sitemap files
 echo "🔄 Adding sitemap files..."
-git add renderer/static/sitemaps/*.xml
+git add /Users/hudsonvandal/Documents/sumtube/BE/renderer/static/sitemaps/*.xml
 
 # 3. Commit
 if git diff --cached --quiet; then
